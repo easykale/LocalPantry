a76b164ef7c193b1e7f70317d73942f1d7f2109d
easykale <thatsomebodywhoseanobody@gmail.com>
2026-02-20 21:55:04 +0000

    syncing

--- Package.swift
+++ Package.swift
@@ -10,9 +10,7 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
             name: "app",
-            exclude: [
-            "ViewModels/ViewModel.swift"
-        ]
+            exclude: []
             )
         
     ]
